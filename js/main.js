911803666adee07ea7e1192e16164f79596ad9b3
unknown <slinhart@knights.ucf.edu>
2016-04-05 20:36:51 -0400

    added `scroll to top` btn

--- js/main.js
+++ js/main.js
@@ -9,4 +9,18 @@ $(function() {
 			$navContainer.addClass('slide-down');
 		}
 	});
+
+	// Watch for scroll to bottom of page
+	$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (screen.height + y === $(this).height()) {
+        $('.scroll-top-btn').fadeIn(900);
+    } else {
+        $('.scroll-top-btn').fadeOut(600);
+    }
+	});
+
+	$('.scroll-top-btn-container').click(function() {
+		window.scrollTo(0,0);
+	});
 });
