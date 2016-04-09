4fbb0c6bf22f5213bea5c2f5076c75298fa480d5
unknown <slinhart@knights.ucf.edu>
2016-04-09 18:43:02 -0400

    added home link to header

--- partials/header.php
+++ partials/header.php
@@ -3,9 +3,11 @@
 		<div class="hamburger-wrapper">
 			<button class="btn hamburger-nav-btn"><i class="fa fa-bars"></i></button>
 		</div>
-		<div class="inline-block full-width h1-padding">
-			<h1 class="primary-header">Shayne Linhart</h1>
-		</div>
+			<div class="inline-block full-width h1-padding">
+				<a href='/' class="home-link">
+					<h1 class="primary-header">Shayne Linhart</h1>
+				</a>
+			</div>
 	</div>
 </header>
 <div class="nav-container">
@@ -15,13 +17,13 @@
 				<li><a>Blog</a></li>
 				<li><a>About</a></li>
 				<li><a class="nav-logo-link">
-					<img class="nav-logo" src="img/logo/github.png" />
+					<img class="nav-logo" src="/img/logo/github.png" />
 				</a></li>
 				<li><a class="nav-logo-link">
-					<img class="nav-logo" src="img/logo/linkedin.png" />
+					<img class="nav-logo" src="/img/logo/linkedin.png" />
 				</a></li>
 				<li><a class="nav-logo-link">
-					<img class="nav-logo" src="img/logo/stackoverflow.png" />
+					<img class="nav-logo" src="/img/logo/stackoverflow.png" />
 				</a></li>
 				<li><a href="/contact">
 					Contact
