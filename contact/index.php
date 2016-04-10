0081683d34bd173c90b08a1838df3d69ceaa7ce8
unknown <slinhart@knights.ucf.edu>
2016-04-09 22:39:11 -0400

    wip contact page

--- contact/index.php
+++ contact/index.php
@@ -6,11 +6,29 @@
 <body>
 	<?php include('../partials/header.php'); ?>
 	<h1 class="send-message-title">Send Message</h1>
-	<form action="message.php" method="post">
-		<input type="text" name="email" />
-		<input type="text" name="message" />
-		<button type="submit">Send</button>
-	</form>
+	<div class="message-form-container">
+		<form action="message.php" method="post" class="form-horizontal message-form">
+			<div class="form-group">
+				<label class="col-xs-3 control-label">Email</label>
+				<div class="col-xs-9">
+					<input type="text" name="email" class="form-control" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-xs-3 control-label">Message</label>
+				<div class="col-xs-9">
+					<textarea type="text" name="message" class="form-control" rows="5"></textarea>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-xs-12">
+					<div class="pull-right">
+						<button class="btn btn-primary" type="submit">Send</button>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
 	<?php include('../partials/primary_scripts.php'); ?>
 </body>
 </html>
