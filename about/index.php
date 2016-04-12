8349e62a28eecfd2ef8afa417728b2ea5245bd29
unknown <slinhart@knights.ucf.edu>
2016-04-11 23:15:47 -0400

    started on about me page

--- about/index.php
+++ about/index.php
@@ -1,11 +1,44 @@
 <html>
 <head>
-	<?php include('/partials/head.php'); ?>
+	<?php include('../partials/head.php'); ?>
 	<link href="/styles/about.css" rel="stylesheet">
 </head>
 <body>
-	<?php include('/partials/header.php'); ?>
-	
-	<?php include('/partials/primary_scripts.php'); ?>
+	<?php include('../partials/header.php'); ?>
+	<div class="content-container">
+		<div class="centered-content">
+			<h1 class="about-me-title">About Me</h1>
+			<p class="about-me-text">
+				Morbi porttitor nunc in lacinia rhoncus. Aliquam quis est ligula. Duis quis mi posuere, consectetur neque luctus, ullamcorper turpis. Vivamus suscipit sodales volutpat. Curabitur quis massa mauris. Sed eu volutpat orci, ac accumsan turpis. Suspendisse sed consectetur dolor. Aliquam a augue ac mauris posuere pharetra. Aliquam erat volutpat. Etiam luctus accumsan nibh eu porta. Nam porttitor enim vitae felis finibus, vel euismod turpis elementum. Phasellus nunc turpis, malesuada eu libero pellentesque, malesuada scelerisque risus. Suspendisse scelerisque nisi luctus, vulputate mi at, molestie eros.
+			</p>
+			<section class="skills-section">
+				<h2 class="skills-title">Skills</h2>
+				<a href="resume-download-link">Download Resume</a>
+				<div>
+					<span class="skill-tag">JavaScript</span>
+					<span class="skill-tag">HTML</span>
+					<span class="skill-tag">CSS</span>
+					<span class="skill-tag">Php</span>
+					<span class="skill-tag">Backbone</span>
+					<span class="skill-tag">jQuery</span>
+					<span class="skill-tag">Sass</span>
+					<span class="skill-tag">Webpack</span>
+					<span class="skill-tag">Marionette</span>
+					<span class="skill-tag">AngularJS</span>
+					<span class="skill-tag">Java</span>
+				</div>
+			</section>
+			<section class="who-im-reading-section">
+				<h3>Who I'm Reading</h3>
+				<ul>
+					<li><a href="http://philipwalton.com/"><span>Philip Walton</span></a></li>
+					<li><a href="http://braddunbar.net/"><span>Brad Dunbar</span></a></li>
+					<li><a href="https://addyosmani.com/blog/"><span>Addy Osmani</span></a></li>
+					<li><a href="http://ejohn.org/category/blog/"><span>John Resig</span></a></li>
+					<li><a href="https://twitter.com/floydophone?lang=en"><span>Pete Hunt</span></a></li>
+				</ul>
+			</section>
+	</div>
+	<?php include('../partials/primary_scripts.php'); ?>
 </body>
 </html>
--- partials/header.php
+++ partials/header.php
@@ -15,7 +15,7 @@
 		<div class="center-nav">
 			<ul>
 				<li><a>Blog</a></li>
-				<li><a>About</a></li>
+				<li><a href="/about">About</a></li>
 				<li><a class="nav-logo-link" href="https://github.com/slinhart" target="_new">
 					<img class="nav-logo" src="/img/logo/github.png" />
 				</a></li>
