e8fd5ab114a4a64dd9fa1b727775ee7c0d1fd594
unknown <slinhart@knights.ucf.edu>
2016-04-27 23:07:17 -0400

    wip responsive styles (navigation)

--- partials/header.php
+++ partials/header.php
@@ -1,12 +1,34 @@
 <header class="header">
 	<div class="fixed-bar">
-		<div class="hamburger-wrapper hidden-sm">
+		<div class="hamburger-wrapper hidden-sm hidden-md hidden-lg">
 			<button class="btn hamburger-nav-btn"><i class="fa fa-bars"></i></button>
 		</div>
 			<div class="inline-block full-width h1-padding">
 				<a href='/' class="home-link">
-					<h1 class="primary-header">Shayne Linhart</h1>
+					<div class="primary-title-wrapper">
+						<h1 class="primary-title">Shayne Linhart</h1>
+					</div>
 				</a>
+				<div class="nav-container">
+					<nav class="main-nav">
+						<ul>
+							<li><a>Blog</a></li>
+							<li><a href="/about">About</a></li>
+							<li><a href="/contact">
+								Contact
+							</a></li>
+							<li><a class="nav-logo-link" href="https://github.com/slinhart" target="_new">
+								<img class="nav-logo" src="/img/logo/github.png" />
+							</a></li>
+							<li><a class="nav-logo-link" href="https://www.linkedin.com/in/shaynelinhart" target="_new">
+								<img class="nav-logo" src="/img/logo/linkedin.png" />
+							</a></li>
+							<li><a class="nav-logo-link" href="http://stackoverflow.com/users/2771143/slinhart" target="_new">
+								<img class="nav-logo" src="/img/logo/stackoverflow.png" />
+							</a></li>
+						</ul>
+					</nav>
+				</div>
 			</div>
 	</div>
 </header>
