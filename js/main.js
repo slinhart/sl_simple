03732fd734f60a956039dce51e01576842ba7a71
unknown <slinhart@knights.ucf.edu>
2016-07-20 17:19:54 -0600

    Added fallback for mobile nav for browsers without transform property... opera mini)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
+	// Detect if transform is supported,
+	// if its not add transform-fallback class to hamburger
+	(function() {
+		var prefixes = ['transform', 'WebkitTransform', 'MozTransform', 'OTransform', 'msTransform'];
+		var div = document.createElement('div');
+
+		// for(p in prefixes) {
+		// 	if(div && div.style[prefixes[p]] !== undefined) {
+		// 		return;
+		// 	}
+		// }
+
+		$('.mobile-nav-container').addClass('transform-fallback');
+	})();
+
 	// Listen for hamburger click
 	$('.hamburger-wrapper').on('click', function(e) {
 		var $navContainer = $('.mobile-nav-container');
